a79dff38539368e45116bf63fab17e9d932563e6
Aleksei Petrogradov <Aleksei.Petrogradov@EKTACO.local>
2019-03-05 18:00:36 +0200

    upload 0.5

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var querystring = require('querystring');
 
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var page = fs.readFileSync('./pages/index.html');
@@ -10,15 +11,30 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent the text: " +  querystring.parse(postData).text);
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("./files/test.jpg", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
   //функции с обработчика
   
